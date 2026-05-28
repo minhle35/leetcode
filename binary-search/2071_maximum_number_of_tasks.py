453d19e16c0fe98fa7aaf9a56984060bae6d5e1d
Thao Minh Le <118033201+minhle35@users.noreply.github.com>
2026-05-28 15:59:58 +1000

    fix(2071): add a set of record of index to keep track of used workers to avoid using again the worker in workers[j] and increment max_tasks incorrectly

--- binary-search/2071_maximum_number_of_tasks.py
+++ binary-search/2071_maximum_number_of_tasks.py
@@ -6,12 +6,17 @@ class Solution:
         self, tasks: List[int], workers: List[int], pills: int, strength: int
     ) -> int:
         max_tasks = 0
+        used_worker = set()
         for i in range(len(tasks)):
             for j in range(len(workers)):
+                if j in used_worker:
+                    continue
                 if workers[j] >= tasks[i]:
+                    used_worker.add(j)
                     max_tasks += 1
                     break
                 elif pills > 0 and strength + workers[j] >= tasks[i]:
+                    used_worker.add(j)
                     pills -= 1
                     max_tasks += 1
                     break
